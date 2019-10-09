5798b77f941277b199aa2c2df2a3a1880f52ea6c
Olimpia Balaci <olimpia.balaci@yahoo.com>
2019-10-09 21:28:07 +0300

    Initial commit - Olimpia

--- src/com/olba/constants/Constants.java
+++ src/com/olba/constants/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     public static final String DB_PASSWORD = "adminAccount";
 
     public static final String DB_DRIVER_IMPLEMENTATION = "jdbc:mysql";
-    public static final String DB_CONN_STRING = "localhost:3306/imdb";
+    public static final String DB_CONN_STRING = "localhost:3306/onlinecatalog";
 }
