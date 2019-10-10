959eea3f50115823f452734dc75d26e34e7704b4
ArianaDM <dumitrescuariana@yahoo.com>
2019-10-10 22:27:26 +0300

    add fields in Schedule- Ariana

--- src/com/olba/model/Schedule.java
+++ src/com/olba/model/Schedule.java
@@ -1,4 +1,14 @@
 package com.olba.model;
 
 public class Schedule {
+    private String classroom_id;
+    private String teacher;
+    private String location;
+    private String date;
+    private String start;
+    private String time_slot;
+    private String details;
+    private String discipline;
+
+
 }
