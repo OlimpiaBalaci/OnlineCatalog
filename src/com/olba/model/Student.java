5ecee25739b71f10881d871191b1c5af918dc3bd
Olimpia Balaci <olimpia.balaci@yahoo.com>
2019-10-10 21:24:27 +0300

    Rename Classes - Olimpia

--- src/com/olba/model/Student.java
+++ src/com/olba/model/Student.java
@@ -4,5 +4,8 @@ public class Student {
     private String first_name;
     private String last_name;
     private String e_mail;
+    private String date_of_birth;
+    private String phone;
+    private String classroom;
 
 }
