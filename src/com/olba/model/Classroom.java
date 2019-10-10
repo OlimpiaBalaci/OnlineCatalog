abd7c49320cbfedfbe785855b6db44676ba3f016
ArianaDM <dumitrescuariana@yahoo.com>
2019-10-10 21:50:08 +0300

    add fields to classroom- Ariana

--- src/com/olba/model/Classroom.java
+++ src/com/olba/model/Classroom.java
@@ -2,4 +2,11 @@ package com.olba.model;
 
 public class Classroom {
 
+    private String class_name;
+    private String students_list;
+    private String teachers_list;
+    private String year;
+
+
+
 }
