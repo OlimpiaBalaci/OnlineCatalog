3e0001cbb2d1162c5433f80a15421cccfec226ff
Olimpia Balaci <olimpia.balaci@yahoo.com>
2019-10-10 21:24:47 +0300

    Rename Classes - Olimpia

--- src/com/olba/model/Teacher.java
+++ src/com/olba/model/Teacher.java
@@ -1,4 +1,10 @@
 package com.olba.model;
 
 public class Teacher {
+    private String first_name;
+    private String last_name;
+    private String e_mail;
+    private String date_of_birth;
+    private String phone;
+    private String classroom;
 }
