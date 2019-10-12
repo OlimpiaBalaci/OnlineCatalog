2e96202c8d3e13ae76d29924d9fe72e7a517be08
Olimpia Balaci <olimpia.balaci@yahoo.com>
2019-10-12 18:05:33 +0300

    Added new student - Olimpia

--- src/com/olba/Main.java
+++ src/com/olba/Main.java
@@ -1,9 +1,17 @@
 package com.olba;
 
+import com.olba.model.Student;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //test
+        // write your code here
+
+        //for student
+        Student student = new Student("Olimpia", "Balaci", "olimpia.balaci@gmail.com", "11/06/1982",
+                "0728478802", "9B", "2019"
+        );
+        String str = student.showStudent();
+        System.out.println(str);
     }
 }
