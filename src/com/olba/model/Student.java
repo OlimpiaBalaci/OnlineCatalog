2b2b745ab30381cdb7944b18c5045ff5cfe999c3
ArianaDM <dumitrescuariana@yahoo.com>
2019-10-12 17:44:45 +0300

    add method in student- Ariana

--- src/com/olba/model/Student.java
+++ src/com/olba/model/Student.java
@@ -8,4 +8,18 @@ public class Student {
     private String phone;
     private String classroom;
     private String year;
+
+    public Student( String first_name, String last_name, String e_mail, String date_of_birth, String phone, String classroom, String year ) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.e_mail = e_mail;
+        this.date_of_birth = date_of_birth;
+        this.phone = phone;
+        this.classroom = classroom;
+        this.year = year;
+    }
+    public String showStudent() {
+
+        return first_name + last_name;
+    }
 }
