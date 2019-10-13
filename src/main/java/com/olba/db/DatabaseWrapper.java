0802f9f1dbe63ee99db95c8b91a78066df45e321
ArianaDM <dumitrescuariana@yahoo.com>
2019-10-13 09:10:42 +0300

    removed DB wrapper content- Ariana

--- src/main/java/com/olba/db/DatabaseWrapper.java
+++ src/main/java/com/olba/db/DatabaseWrapper.java
@@ -1,14 +1,5 @@
 package com.olba.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class DatabaseWrapper {
-    private Connection connection;
 
-    public Connection createConnection (String url, String user, String password) throws SQLException {
-        connection = DriverManager.getConnection(url,user,password);
-        return connection;
-    }
 }
