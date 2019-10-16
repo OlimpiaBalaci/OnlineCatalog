d81ce2826d03e447999d209376a6de849767dd6c
Olimpia Balaci <olimpia.balaci@yahoo.com>
2019-10-16 23:57:38 +0300

    Adding TeacherRepository class in repository - Olimpia

---
+++ src/main/java/com/olba/repository/TeacherRepository.java
@@ -0,0 +1,38 @@
+package com.olba.repository;
+
+import com.olba.model.Student;
+import com.olba.model.Teacher;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class TeacherRepository implements CrudRepository<Teacher, Integer> {
+
+    public TeacherRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    private EntityManager entityManager;
+
+    @Override
+    public Optional<Teacher> findById(Integer id){
+        return Optional.empty();
+    }
+@Override
+public List<Teacher> findAll(){
+        return null;
+}
+    @Override
+    public Optional<Teacher> save(Teacher teacher){
+        entityManager.getTransaction().begin();
+        entityManager.persist(teacher);
+        entityManager.getTransaction().commit();
+        return Optional.empty();
+    }
+
+    @Override
+    public Teacher deleteById(Integer id){
+        return null;
+    }
+}
